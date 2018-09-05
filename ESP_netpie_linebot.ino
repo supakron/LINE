7eb504e208a42befb0d3d126e57f19577aa4f185
supakron <supakronjaitham@gmail.com>
2018-09-05 22:04:16 +0700

    Update ESP_netpie_linebot.ino

--- ESP_netpie_linebot.ino
+++ ESP_netpie_linebot.ino
@@ -2,15 +2,15 @@
 #include <MicroGear.h>
 #include <ESP8266HTTPClient.h>
 #include <ArduinoJson.h>
-const char* ssid     = "your SSID"; //change this to your SSID
-const char* password = "your PASSWORD"; //change this to your PASSWORD
+const char* ssid     = "USER1"; //change this to your SSID
+const char* password = "88888888"; //change this to your PASSWORD
 
-const char* host = "your linebot server";//change this to your linebot server ex.http://numpapick-linebot.herokuapp.com/bot.php
-#define APPID   "your APPID"     //change this to your APPID
-#define KEY     "your KEY"     //change this to your KEY
-#define SECRET  "your SECRET"     //change this to your SECRET
+const char* host = "http://homewisdom.herokuapp.com/bot.php";//change this to your linebot server ex.http://numpapick-linebot.herokuapp.com/bot.php
+#define APPID   "WISDOMLINE"     //change this to your APPID
+#define KEY     "nTlh0KEizxd2Z1q"     //change this to your KEY
+#define SECRET  "4WfgeA1v08BrYlsPzGpLLWlVd"     //change this to your SECRET
 
-#define ALIAS   "NodeMCU1" //set name of drvice
+#define ALIAS   "NodeNoTNOS" //set name of drvice
 #define TargetWeb "switch" //set target name of web
 
 WiFiClient client;
