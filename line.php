95b0ada2b86794a4edf77048262dbf58fa768bb9
supakron <supakronjaitham@gmail.com>
2018-09-05 21:57:37 +0700

    Update line.php

--- line.php
+++ line.php
@@ -2,7 +2,8 @@
   
 
 function send_LINE($msg){
- $access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU='; 
+ $access_token = 'ww1zZVt7iJwXZxE9y6+Q75kTSmyIV71i+awke6AGS/BoaLjMriym7n0inJfYLaylgWGee6E5XmMfIa/oM/YCKF3HW12G0X6LAtNgqygrYW+4BANxam6T/PNdUKE17bvLjb7hQrXbZAw9j+/0ZMq5lAdB04t89/1O/w1cDnyilFU=
+'; 
 
   $messages = [
         'type' => 'text',
@@ -14,7 +15,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => 'Ue77a191627f6ac91899e75d92264310c',
+        'to' => 'U09e1a6329958899387ec623fc038b10e',
         'messages' => [$messages],
       ];
       $post = json_encode($data);
